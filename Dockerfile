6cf53cd2a1ef1da9a974457e5ba14a1badad75ae
RJ Trujillo <eyecantcu@pm.me>
2023-12-24 20:56:26 -0700

    chore: Revert to composite action

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ghcr.io/eyecantcu/podman-minimal:latest
-
-COPY . .
-
-ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-podman exec -e PRIVKEY=$PRIVKEY -e PUBKEY=$PUBKEY -w $PWD --privledged $IMAGE ./sign-kernel.sh
